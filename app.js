18272b478fd79bb97a417dd25aafc0c9731c617b
Fahima-anny <fahima.info03@gmail.com>
2024-09-23 21:48:23 +0600

    fourth commit

--- app.js
+++ app.js
@@ -1,23 +1,92 @@
-// console.log(Date())
 let historyBtn = document.getElementById("historyBtn") ;
 let donationBtn = document.getElementById("donationBtn") ;
+let totalAccountBalance = document.getElementById('totalAccountBalance') ;
+let floodNoakhaliTotal = document.getElementById('floodNoakhaliTotal') ;
+let floodFeniTotal = document.getElementById('floodFeniTotal') ;
+let quotaTotal = document.getElementById('quotaTotal') ;
 
+// history button 
 historyBtn.addEventListener("click",function(){
-    displayDiv('historySection') ;
-
+    displayDiv(historySection) ;
     historyBtn.classList.add('bg-[#B4F461]','hover:bg-[#b4f461c4]')
     historyBtn.classList.remove('hover:bg-[#f4f4f4c4]','text-gray-500','ring-2')
     donationBtn.classList.remove('bg-[#B4F461]','hover:bg-[#b4f461c4]')
     donationBtn.classList.add('text-gray-500','ring-2')
-
 })
 
+// donate button 
 donationBtn.addEventListener("click",function(){
-    displayDiv('cardSection') ;
-
+    displayDiv(cardSection) ;
     donationBtn.classList.add('bg-[#B4F461]','hover:bg-[#b4f461c4]')
     donationBtn.classList.remove('text-gray-500','ring-2')
     historyBtn.classList.remove('bg-[#B4F461]','hover:bg-[#b4f461c4]')
     historyBtn.classList.add('hover:bg-[#f4f4f4c4]','text-gray-500','ring-2')
-
 })
+
+
+// flood noakhali part 
+document.getElementById("floodNoakhaliBtn").addEventListener('click',function(){
+let Amount = getValue('floodNoakhaliInput') ;
+let prevAccountBalance = getInnerText(totalAccountBalance)
+if(isNaN(Amount) || Amount<0){
+    alert("Please give a valid input")
+    return ;
+}
+if(Amount>prevAccountBalance){
+    alert('Donation amount is much than your account balance') ;
+    return ;
+}
+let floodBalance = getInnerText(floodNoakhaliTotal) ;
+floodNoakhaliTotal.innerText = floodBalance + Amount ;
+totalAccountBalance.innerText = prevAccountBalance - Amount ;
+addInHistory(Amount,'noakhaliHeading') ;
+document.getElementById('my_modal_1').showModal()
+})
+
+
+// flood feni part 
+document.getElementById("floodFeniBtn").addEventListener('click',function(){
+let Amount = getValue('floodFeniInput') ;
+let prevAccountBalance = getInnerText(totalAccountBalance)
+if(isNaN(Amount) || Amount<0){
+    alert("Please give a valid input")
+    return ;
+}
+if(Amount>prevAccountBalance){
+    alert('Donation amount is much than your account balance') ;
+    return ;
+}
+let floodBalance = getInnerText(floodFeniTotal) ;
+floodFeniTotal.innerText = floodBalance + Amount ;
+totalAccountBalance.innerText = prevAccountBalance - Amount ;
+addInHistory(Amount,'feniHeading') ;
+document.getElementById('my_modal_1').showModal()
+})
+
+
+// quota part 
+document.getElementById("quotaBtn").addEventListener('click',function(){
+let Amount = getValue('quotaInput') ;
+let prevAccountBalance = getInnerText(totalAccountBalance)
+if(isNaN(Amount) || Amount<0){
+    alert("Please give a valid input")
+    return ;
+}
+if(Amount>prevAccountBalance){
+    alert('Donation amount is much than your account balance') ;
+    return ;
+}
+let quotaBalance = getInnerText(quotaTotal) ;
+quotaTotal.innerText = quotaBalance + Amount ;
+totalAccountBalance.innerText = prevAccountBalance - Amount ;
+addInHistory(Amount,'quotaHeading') ;
+document.getElementById('my_modal_1').showModal()
+})
+
+
+
+
+
+
+
+
--- utilities.js
+++ utilities.js
@@ -1,18 +1,39 @@
+// toggle button 
 function displayDiv (sectionId) {
     cardSection.style.display = 'none'
     historySection.style.display = 'none'
 
-    document.getElementById(sectionId).style.display = 'block'
-
+    sectionId.style.display = 'block'
 }
 
 
+// for input value 
+function getValue (id){
+    let idValue = document.getElementById(id).value ;
+    let idValueFloat = parseFloat(idValue) ;
+    document.getElementById(id).value = ''
+    return idValueFloat ;
+}
+
+// for inner text 
+function getInnerText(id){
+    let text = id.innerText ;
+    let textFloat = parseFloat(text) ;
+    return textFloat ;
+}
 
 
-
-
-
-
+// for history append 
+function addInHistory (amount,heading) {
+    let header = document.getElementById(heading).innerText
+let historySection = document.getElementById('historySection') ;
+let newDiv = document.createElement('div') ;
+newDiv.innerHTML = `<div class="border rounded-2xl p-8 flex gap-5 flex-col mt-10">
+    <h1 class="font-bold text-2xl">${amount} Taka is Donated for ${header}</h1>
+    <p  class="text-lg font-light text-gray-500"  >Date : ${Date()}</p>
+</div>`
+historySection.appendChild(newDiv) ;
+}
 
 
 
