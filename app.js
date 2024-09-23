107b5c0e38a541c3f4b67c5cdc368c7392826e81
Fahima-anny <fahima.info03@gmail.com>
2024-09-23 17:54:00 +0600

    third commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(Date())
+// console.log(Date())
+let historyBtn = document.getElementById("historyBtn") ;
+let donationBtn = document.getElementById("donationBtn") ;
+
+historyBtn.addEventListener("click",function(){
+    displayDiv('historySection') ;
+
+    historyBtn.classList.add('bg-[#B4F461]','hover:bg-[#b4f461c4]')
+    historyBtn.classList.remove('hover:bg-[#f4f4f4c4]','text-gray-500','ring-2')
+    donationBtn.classList.remove('bg-[#B4F461]','hover:bg-[#b4f461c4]')
+    donationBtn.classList.add('text-gray-500','ring-2')
+
+})
+
+donationBtn.addEventListener("click",function(){
+    displayDiv('cardSection') ;
+
+    donationBtn.classList.add('bg-[#B4F461]','hover:bg-[#b4f461c4]')
+    donationBtn.classList.remove('text-gray-500','ring-2')
+    historyBtn.classList.remove('bg-[#B4F461]','hover:bg-[#b4f461c4]')
+    historyBtn.classList.add('hover:bg-[#f4f4f4c4]','text-gray-500','ring-2')
+
+})
---
+++ utilities.js
@@ -0,0 +1,30 @@
+function displayDiv (sectionId) {
+    cardSection.style.display = 'none'
+    historySection.style.display = 'none'
+
+    document.getElementById(sectionId).style.display = 'block'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
